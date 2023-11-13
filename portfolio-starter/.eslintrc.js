2b29c08e7111463fd07dcd58dc19ae7490aa94ae
RitvikY <ritviky@terpmail.umd.edu>
2023-11-13 13:20:03 -0500

    new changes

--- portfolio-starter/.eslintrc.js
+++ portfolio-starter/.eslintrc.js
@@ -1,10 +1,19 @@
+const standard = require("eslint-config-standard");
 module.exports = {
-  "extends": "airbnb-base",
-  "env": {
-    "browser": true,
-    "node": true
+  extends: ["standard"],
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: "module",
   },
   rules: {
     "no-console": "off",
-  }
+    "linebreak-style": 0,
+    "no-param-reassign": [2, { props: false }],
+    "no-unused-vars": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+  },
 };
--- portfolio-starter/src/javascripts/index.js
+++ portfolio-starter/src/javascripts/index.js
@@ -1,52 +1,73 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Card expansion functionality (Projects and Experience)
-    const cards = document.querySelectorAll('.card, .experience-card');
-    cards.forEach(card => {
-        card.addEventListener('click', function () {
-            const overlay = document.querySelector('.overlay-background');
-            const expandedContent = card.querySelector('.expanded-content');
-            if (!card.classList.contains('expanded')) {
-                // Close any other expanded card
-                const expandedCard = document.querySelector('.card.expanded, .experience-card.expanded');
-                if (expandedCard) {
-                    expandedCard.classList.remove('expanded');
-                    const otherExpandedContent = expandedCard.querySelector('.expanded-content');
-                    if (otherExpandedContent) {
-                        otherExpandedContent.classList.add('hidden');
-                        otherExpandedContent.style.display = ''; // Reset display property
-                    }
-                }
-                // Expand the clicked card
-                card.classList.add('expanded');
-                if (expandedContent) {
-                    expandedContent.classList.remove('hidden');
-                    expandedContent.style.display = 'block'; // Ensure it's visible
-                }
-                overlay.classList.add('show');
-            } else {
-                // Close the expanded card
-                card.classList.remove('expanded');
-                if (expandedContent) {
-                    expandedContent.classList.add('hidden');
-                    expandedContent.style.display = ''; // Reset display property
-                }
-                overlay.classList.remove('show');
-            }
-        });
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  // Card expansion functionality (Projects and Experience)
+  const cards = document.querySelectorAll('.card, .experience-card')
+  cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      const overlay = document.querySelector('.overlay-background')
+      if (!overlay) {
+        console.error('Overlay element not found!')
+        return
+      }
 
-    // Overlay functionality
-    const overlay = document.querySelector('.overlay-background');
-    overlay.addEventListener('click', function () {
-        const expandedCard = document.querySelector('.card.expanded, .experience-card.expanded');
+      const expandedContent = card.querySelector('.expanded-content')
+      if (!expandedContent) {
+        console.error('Expanded content not found in card!')
+        return
+      }
+
+      if (!card.classList.contains('expanded')) {
+        // Close any other expanded card
+        const expandedCard = document.querySelector('.card.expanded, .experience-card.expanded')
         if (expandedCard) {
-            expandedCard.classList.remove('expanded');
-            const expandedContent = expandedCard.querySelector('.expanded-content');
-            if (expandedContent) {
-                expandedContent.classList.add('hidden');
-                expandedContent.style.display = ''; // Reset display property
-            }
+          expandedCard.classList.remove('expanded')
+          const otherExpandedContent = expandedCard.querySelector('.expanded-content')
+          if (otherExpandedContent) {
+            otherExpandedContent.classList.add('hidden')
+            otherExpandedContent.style.display = ''
+          }
         }
-        overlay.classList.remove('show');
-    });
-});
+        // Expand the clicked card
+        card.classList.add('expanded')
+        expandedContent.classList.remove('hidden')
+        expandedContent.style.display = 'block'
+        overlay.classList.add('show')
+      } else {
+        // Close the expanded card
+        card.classList.remove('expanded')
+        expandedContent.classList.add('hidden')
+        expandedContent.style.display = ''
+        overlay.classList.remove('show')
+      }
+    })
+  })
+
+  // Overlay functionality
+  const overlay = document.querySelector('.overlay-background')
+  if (!overlay) {
+    console.error('Overlay element not found!')
+  } else {
+    overlay.addEventListener('click', () => {
+      const expandedCard = document.querySelector('.card.expanded, .experience-card.expanded')
+      if (expandedCard) {
+        expandedCard.classList.remove('expanded')
+        const expandedContent = expandedCard.querySelector('.expanded-content')
+        if (expandedContent) {
+          expandedContent.classList.add('hidden')
+          expandedContent.style.display = ''
+        }
+      }
+      overlay.classList.remove('show')
+    })
+  }
+
+  // Form submission handling
+  const contactForm = document.getElementById('contact-form')
+  if (contactForm) {
+    contactForm.addEventListener('submit', (event) => {
+      event.preventDefault()
+      console.log('Form submission triggered')
+    })
+  } else {
+    console.error('Contact form not found!')
+  }
+})
